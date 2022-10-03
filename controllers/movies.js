5f80b87ba834e63e77024b3bc91a795b8446fddf
Vlad <borovkov-vlad@ya.ru>
2022-10-03 12:10:55 +0600

    feat: правки после первого ревью

--- controllers/movies.js
+++ controllers/movies.js
@@ -11,34 +11,7 @@ module.exports.getAllUserMovie = (req, res, next) => {
 };
 
 module.exports.createMovie = (req, res, next) => {
-  const {
-    country,
-    director,
-    duration,
-    year,
-    description,
-    image,
-    trailerLink,
-    thumbnail,
-    movieId,
-    nameRU,
-    nameEN,
-  } = req.body;
-
-  Movie.create({
-    country,
-    director,
-    duration,
-    year,
-    description,
-    image,
-    trailerLink,
-    thumbnail,
-    owner: req.user._id,
-    movieId,
-    nameRU,
-    nameEN,
-  })
+  Movie.create({ ...req.body, owner: req.user._id })
     .then((movie) => res.send({ movie }))
     .catch((err) => {
       if (err.name === "ValidationError") {
